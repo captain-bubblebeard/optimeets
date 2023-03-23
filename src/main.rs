d98cb74fa08b6586420b94c49839893fa52eb84e
captain-bubblebeard <tyumol@yandex.ru>
2023-03-23 21:34:35 +0300

    Toying with ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ndarray::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut a: Array2<i32> = Array::zeros((2,2));
+
+    for i in 0..a.nrows() {
+        for j in 0..a.ncols() {
+            a[[i,j]] += 1;
+        }
+    }
+
+    println!("{:?}", a);
 }
